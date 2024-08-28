60e98842e0c2b7c8861c0750c52cf7d123189a4d
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-08-28 12:48:18 +0100

    Refactor moving upper method to word variable

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ nato_alphabet = read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in nato_alphabet.iterrows()}
 
 # Ask user for a word and return each letter as NATO alphabet
-input_word = input("Enter a word: ")
-output_list = [nato_dict[letter.upper()] for letter in input_word]
+input_word = input("Enter a word: ").upper()
+output_list = [nato_dict[letter] for letter in input_word]
 print(output_list)
 
 
